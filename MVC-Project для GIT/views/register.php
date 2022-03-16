214d3fe655cef7bfa976de14e5d97c2407a0c651
Osore <l-d-03-03@mail.ru>
2022-03-16 11:36:49 +0500

    version 1.0.3

--- MVC-Project для GIT/views/register.php
+++ MVC-Project для GIT/views/register.php
@@ -11,11 +11,24 @@
     : "" )
 ?>
 <form action="" method="POST">
-    <input type="text" name="name" placeholder="Фамилия Имя Отчество" required>
-    <input type="text" name="login" placeholder="Укажите логин:" required>
-    <input type="password" name="password" placeholder="Укажите пароль:" required>
-    <input type="password" name="password_confirmed" placeholder="Укажите пароль повторно:" required>
-    <input type="submit" value="Регистрация">
-</form>
+        <div class="mb-3">
+            <label for="inputName" class="form-label">ФИО</label>
+            <input type="text" name="name" class="form-control" id="inputName" placeholder="Фамилия Имя Отчество" required>
+        </div>
+        <div class="mb-3">
+            <label for="inputLogin" class="form-label">Логин</label>
+            <input type="text" name="login" class="form-control" id="inputLogin" placeholder="Укажите логин" required>
+        </div>
+        <div class="mb-3">
+            <label for="inputPassword" class="form-label">Пароль</label>
+            <input type="password" name="passsword" class="form-control" id="inputPassword" placeholder="Укажите пароль" required>
+        </div>
+        <div class="mb-3">
+            <label for="inputPasswordConfirmed" class="form-label">Повтолите пароль</label>
+            <input type="password" name="passsword_confirmed" class="form-control" id="inputPasswordConfirmed" placeholder="Повтолите пароль" required>
+        </div>
+        <button type="submit" class="btn btn-primary">Ок</button>
+    </form>
+
 
 <?php @include_once 'footer.php'; ?>
